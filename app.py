c3f132f7ad3b1f43c581b45c2c26fbf80cb1731f
redad76 <derraz.mohamedreda@gmail.com>
2022-12-11 14:41:42 +0100

    Test endpoint api

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import requests
 
-st.write(""" hello world""")
+response = requests.get('https://p9function.azurewebsites.net/api/recommandation?user_id=600')
+
+if response.status_code == 200:
+    print("Succesful connection with API.")
+    print('-------------------------------')
+    data = response.json()
+    print(data)
+elif response.status_code == 404:
+    print("Unable to reach URL.")
+else:
+    print("Unable to connect API or retrieve data.")
+
+st.write(response)
